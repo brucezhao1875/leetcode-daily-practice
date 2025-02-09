5f5e0181659fab5c92d568bf4855e47e635c1092
bruce zhao <13052020@qq.com>
2025-02-09 11:28:24 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -1,18 +1,11 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         n = len(nums)
-        bad_pairs = 0
-        diff_count = {}
-        for pos in range(n):
-            diff = pos - nums[pos]
-
-            #count of previous positions with same difference
-            good_pairs_count = diff_count.get(diff,0)
-
-            #total possible pairs minus good pairs = bad pairs
-            bad_pairs += pos - good_pairs_count
-
-            #update count of positions with this difference
-            diff_count[diff] = good_pairs_count + 1
+        cnt = Counter()
+        good_pairs = 0
+        for i,num in enumerate(nums):
+            diff = num - i
+            good_pairs = good_pairs + cnt[diff]
+            cnt[diff] += 1
         
-        return bad_pairs
+        return (n-1)*2//2 - good_pairs 
