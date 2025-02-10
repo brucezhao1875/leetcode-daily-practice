ee54cee082cad70156f066d0195607582f75e84e
bruce zhao <13052020@qq.com>
2025-02-10 08:30:32 +0800

    Time: 209 ms (23.2%), Space: 28.8 MB (21.6%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -7,9 +7,16 @@ class Solution:
                 x = x[1:]
             return int(''.join(x))
         
+        def rev2(num):
+            result = 0
+            while num:
+                result = result * 10 + num % 10
+                num = num //10
+            return result
+
         n = len(nums)
         
-        diff_nums = [ (num - rev(num)) for num in nums ]
+        diff_nums = [ (num - rev2(num)) for num in nums ]
         
         cnt = Counter()
         result = 0
