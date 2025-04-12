801738fe246a6bd2f93d3deb32db30d4d7e91d3d
bruce zhao <13052020@qq.com>
2025-04-12 10:09:04 +0800

    Time: 0 ms (100%), Space: 17.5 MB (99.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        p = m+n-1
-        p1 = m-1 
-        p2 = n-1
+        p,p1,p2 = m+n-1,m-1,n-1
         while p1>=0 and p2>=0 :
             if nums1[p1]>nums2[p2]:
                 nums1[p] = nums1[p1]
@@ -14,5 +12,4 @@ class Solution:
         
         if p2>=0:
             nums1[0:p2+1] = nums2[0:p2+1]
-        print(nums1)
         
