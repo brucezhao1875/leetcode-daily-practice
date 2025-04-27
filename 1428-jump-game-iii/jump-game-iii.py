54ce2e125895fe6c0b979848e5e6e76b6a24a302
bruce zhao <13052020@qq.com>
2025-04-27 22:58:34 +0800

    Time: 17 ms (22.11%) | Memory: 23.3 MB (47.41%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -1,7 +1,7 @@
 class Solution:
     # 维持一个stat[]，其中元素状态：未处理(0)/已处理(1)
     # 维持一个reach[]，其中元素状态：不可达(0)/可达(1)
-    # 返回结果是：reach和arr中不能有同时为0的位置 -- 实现语法： min ( [ reach [i] or arr[i] for i in range(n) ] ) != 0  
+    # 返回结果是： return any(not arr[i] and reach[i] for i in range(n))
     def canReach(self, arr: List[int], start: int) -> bool:
         debug = 0
         n = len(arr)
@@ -23,7 +23,6 @@ class Solution:
                 stat[index] = 1
             debug and print("x:",x,";stat:",stat,";reach:",reach,";dq:",dq) 
         
-        for _ in range(n):
-            if not arr[_] and reach[_]:
-                return True
-        return False
+        return any(not arr[i] and reach[i] for i in range(n))
+
+
