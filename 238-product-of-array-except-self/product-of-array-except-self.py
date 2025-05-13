c3dc164a25ab59a8fdf4651b56ef2993890444ca
bruce zhao <13052020@qq.com>
2025-05-13 11:24:51 +0800

    Time: 27 ms (47.71%) | Memory: 23.4 MB (53.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,5 +1,19 @@
 class Solution:
+    #这个方法借助了prefix和suffix，看起来轻巧一些。
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1 for _ in range(n)]
+        prefix = 1
+        suffix = 1
+        for i in range(n):
+            result[i] *= prefix
+            prefix *= nums[i]
+        for i in range(n-1,-1,-1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        return result
+
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
         n = len(nums)
         result = [0 for _ in range(n)]
         zero_indices = [index for index,num in enumerate(nums) if num==0]
