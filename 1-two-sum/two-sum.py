9731f167126dbf853b1a464b0779045bac213b7c
bruce zhao <13052020@qq.com>
2025-06-02 13:07:55 +0800

    Time: 3 ms (50.75%) | Memory: 18.8 MB (51.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # 创建一个数组，保存原始索引
         indices = list(range(len(nums)))
-        # 根据 nums 的值排序 indices
-        indices.sort(key=lambda i: nums[i])
-
-        low = 0
-        high = len(nums) - 1
-
-        while low < high:
-            sum_val = nums[indices[low]] + nums[indices[high]]
-            if sum_val == target:
-                return [indices[low], indices[high]]
-            elif sum_val < target:
-                low += 1
+        indices.sort(key = lambda x : nums[x])
+        left,right = 0,len(nums)-1
+        while left<right:
+            sum = nums[indices[left]] + nums[indices[right]]
+            if sum == target:
+                return [indices[left],indices[right]]
+            elif sum < target:
+                left += 1
             else:
-                high -= 1
+                right -= 1
+
