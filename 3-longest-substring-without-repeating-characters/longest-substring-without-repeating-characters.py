f73f12ebe37353e034d30decf3fcd16453ea5aab
bruce zhao <13052020@qq.com>
2025-07-20 13:40:49 +0800

    Time: 19 ms (54.86%) | Memory: 18.1 MB (12.17%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,8 +5,8 @@ class Solution:
         left = 0
         for right in range(len(s)):
             c = s[right]
-            if c in char_index and char_index[c] >= left:
-                left = char_index[c] + 1
+            if c in char_index :
+                left = max(left,char_index[c] + 1)
             char_index[c] = right
             result = max(result,right-left+1)
 
