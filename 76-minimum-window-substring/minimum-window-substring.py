050e195744cea0f8a55c6326dd097bb2b061bdfb
bruce zhao <13052020@qq.com>
2025-07-20 19:02:03 +0800

    Time: 93 ms (44.20%) | Memory: 18.2 MB (77.48%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,55 +1,24 @@
 class Solution:
+
     def minWindow(self, s: str, t: str) -> str:
         need_features = collections.Counter(t)
-        window_features = collections.Counter()
-        valid = 0
+        real_features = collections.Counter()
+        valid = 0 # 符合条件的字符类计数。当 valid==len(need_features)时，表示当前的窗口符合所需特征
         start,end = 0, max(len(s),len(t)) + 1
         left = 0
         for right in range(len(s)):
-            ch = s[right]
-            if need_features[ch] : 
-                window_features[ch] += 1
-                if window_features[ch] == need_features[ch]:
-                    valid += 1
+            x = s[right]
+            if need_features[x] > 0 : 
+                real_features[x] += 1
+                if real_features[x] == need_features[x] : valid += 1  #突变
             while valid == len(need_features):
-                if right-left < end-start:
-                    start,end = left,right
-                ch = s[left]
+                if right-left < end - start : end,start = right,left
+                x = s[left]
                 left += 1
-                if need_features[ch] > 0:
-                    if window_features[ch] == need_features[ch]:
-                        valid -= 1
-                    window_features[ch] -= 1
-        if end == max(len(s),len(t)) + 1 :
+                if need_features[x] > 0 :
+                    if real_features[x] == need_features[x] : valid -= 1  #突变
+                    real_features[x] -= 1
+        if end == max(len(s),len(t)) + 1 : 
             return ""
-        else:
-            return s[start:end+1]        
-
-
-
-    def minWindow2(self, s: str, t: str) -> str:
-        need_features = collections.Counter(t)
-        window_features = collections.Counter()
-        valid = 0 # 多少类字母匹配好了,valid == len(need_features)时，表示完全匹配
-        start,end = 0,max(len(s),len(t))+1
-
-        left = 0
-        for right in range(len(s)):
-            ch = s[right]
-            if need_features[ch]:
-                window_features[ch] += 1
-                if window_features[ch] == need_features[ch]: #在变化的开关瞬间，进行一次valid++的运算
-                    valid += 1
-            while valid == len(need_features) :
-                if right-left< end-start:
-                    end,start = right,left
-                ch = s[left]
-                left += 1
-                if need_features[ch] > 0:
-                    if window_features[ch] == need_features[ch]:
-                        valid -= 1 #在变化的开关瞬间，进行一次valid--的运算
-                    window_features[ch] -= 1
-        if end == max(len(s) , len(t)) + 1:
-            return ""
-        else:
-            return s[start:end+1]            
+        else :
+            return s[start:end+1]
