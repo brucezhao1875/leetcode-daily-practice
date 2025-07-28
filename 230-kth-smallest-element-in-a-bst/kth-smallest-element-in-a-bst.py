82deae677bac535528dc360921821ac5d8009c5b
bruce zhao <13052020@qq.com>
2025-07-28 21:47:04 +0800

    Time: 3 ms (37.07%) | Memory: 21.1 MB (27.32%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        '''
+        这把它遍历出来，然后取第k个，不就可以了吗?
+        '''
+        array = []
+        def traverse(node : Optional[TreeNode],result) -> List[int]:
+            if node is not None :
+                traverse(node.left,array)
+                array.append(node.val)
+                traverse(node.right,array)
+
+        traverse(root,array)
+        print(array)
+        return array[k-1]
