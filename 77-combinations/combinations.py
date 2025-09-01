59a4adce0ad555dd45fcaf30e5f56704d0dd5fdf
bruce zhao <13052020@qq.com>
2025-09-02 00:40:26 +0800

    Time: 134 ms (67.52%) | Memory: 59.6 MB (87.69%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        def backtrace(start, path):
+            if len(path) == k :
+                result.append(path[:])
+                return
+            
+            for i in range(start, n + 1) :
+                path.append(i)
+                backtrace(i + 1, path)
+                path.pop()
+        
+        backtrace(1,[])
+        return result
